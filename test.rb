87cdb4c51e583e25e285a1ad12615bc67c8a90fb
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-13 10:24:07 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,4 +3,14 @@ var2 = 3 && var1
 
 p var2
 
+source_array = [ false, nil, nil, nil, true]
 
+def newmethod(array)
+  return_value = nil
+  array.length.times do |index|
+    return_value = yield(return_value, array[index])
+  end
+  return_value
+end
+
+newmethod(source_array){|memo,n| memo || n }
