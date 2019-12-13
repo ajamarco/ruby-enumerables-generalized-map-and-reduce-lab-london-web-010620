900c2a6ede8cc7765ed6f98601c97286ba70997f
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-13 10:25:40 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -13,4 +13,15 @@ def newmethod(array)
   return_value
 end
 
+def newermethod(array)
+  return_value = array[0]
+  index = 1
+  while index < array.length do
+    return_value = yield(return_value, array[index])
+    index += 1
+  end 
+  return_value
+end
+
+
 newmethod(source_array){|memo,n| memo || n }
