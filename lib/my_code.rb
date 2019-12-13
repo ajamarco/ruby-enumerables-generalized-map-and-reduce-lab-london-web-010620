87984d672e0ffc86d5a0c3f1b21b6feec6e7b1d7
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-13 10:07:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,18 +7,14 @@ def map(source_array)
   total_array
 end 
 
-def reduce(source_array, starting_number = nil)
-  if starting_number
-    num_1 = starting_number
-    i = 0 
-  else
-    num_1 = source_array[0]
-    i = 1
+def reduce(source_array, starting_number = 0)
+  returned_value = 0
+  if starting_number == 0 
+    source_array.length.times do |index|
+      returned_value = yield(returned_value, source_array[index])
+    end 
+    
   end
-  while i < source_array.length do
-    num_1 = yield(num_1, source_array[i])
-    i += 1
-  end
-  num_1
+  returned_value
 end 
 
